2c41cd1a7c20d9c0ee6ce7e181dccde818c8266e
[Nlansong] <[brightwumbei2018@gmail.com]>
2023-03-30 06:29:17 -0700

    main

--- services/index.js
+++ services/index.js
@@ -1,4 +1,3 @@
-import { graphql } from 'graphql';
 import { request, gql } from 'graphql-request';
 const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
 
@@ -90,25 +89,26 @@ export const getRecentPosts = async() =>{
 }
 
 //query for similar posts
-export const getSimilarPosts = async() =>{
-    const query = gql `
-    query GetPostDetails($slug: String, $categories: [String!]) {
-        posts(
-            where: {$slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
-            last: 3
-        ) {
-            title
-            featuredImage{
-                url
-            }
-            createdAt
-            slug
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
         }
+        createdAt
+        slug
+      }
     }
-    `;
-    const result = await request(graphqlAPI, query, {categories, slug});
-    return result.posts;
-}
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
+
+  return result.posts;
+};
 
 //query for listing all category post
 export const getCategories = async() => {
