6fade2d3c1bbc1ac3af4464bfa1c724fc1ae2ada
Nlansong <brightwumbei2018@gmail.com>
2024-08-24 23:00:45 +0000

    added projects page

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,6 @@ app_name = 'home'
 urlpatterns = [
     path('', views.index, name='index'),
     path('project/<int:pk>', views.project, name='project'),
+    path('projects/', views.projects, name='projects'),
     path('about/', views.about, name='about'),
 ]
--- home/views.py
+++ home/views.py
@@ -18,3 +18,10 @@ def project(request, pk):
 
 def about(request):
     return render(request, 'about.html')
+
+def projects(request):
+    projects = Project.objects.all()[:6]
+    context = {
+        'projects':projects
+    }
+    return render(request, 'projects.html', context)
